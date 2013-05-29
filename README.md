5a0396de4e6daf99300425189a77c75ce2e8fc52
Luzifer1984 <luzifer1984@gmail.com>
2013-05-29 16:59:38 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,122 @@
 android_device_htc_chacha
 =========================
 
-Device configuration for HTC ChaCha a810e / Status
+Copyright (C) 2011 The CyanogenMod Project
+Copyright (C) 2011 Andreas Schneider <asn@cryptomilk.org>
+Copyright (C) 2013 XDA users
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+
+CyanogenMod 10.1 device configuration for HTC ChaCha a810e / Status
+
+  It is still a work in progress.
+
+Based on Andreas Schneider sources 
+
+Forked form OWLProject's github
+
+c[device_htc_marvel](http://git.cryptomilk.org/projects/marvel/android_device_htc_marvel.git/)
+
+c[device_htc_marvel-common](http://git.cryptomilk.org/projects/marvel/android_device_htc_marvel-common.git/)
+
+c[device_htc_msm7x27-common](http://git.cryptomilk.org/projects/marvel/android_device_htc_msm7x27-common.git/)
+
+
+[OWLProject's github](https://github.com/OWLProject/android_device_htc_marvel)
+
+
+  - 	bin 	 
+	idc 	 
+  -	key 	 
+  -	Android.mk 	 
+  -	AndroidBoard.mk 	 
+  -	AndroidProducts.mk 	 
+  -	BoardConfig.mk 	 
+  -	CleanSpec.mk 	 
+  -	README 	 
+  -	README.md 	 
+  -	board-info.txt 	 
+  -	cm.dependencies 	 
+  -	cm.mk 	 
+  -	device_chacha.mk 	 
+	init.chacha.rc 	 
+  -	postrecoveryboot.sh 	 
+  -	recovery.fstab 	 
+   	system.prop 	 
+  -	ueventd.chacha.rc 	 
+  -	vendorsetup.sh 	 
+  -	vold.fstab 	 
+
+
+
+* How To Build CM10.1 for HTC chacha:
+---------------------------------------
+
+~~~~~~~~~~~~~¿ Your first build ?~~~~~~~~~~~~~~~~~~~~~~~~~
+
+http://source.android.com/source/initializing.html
+
+Follow steps until Number 5 in the following thread
+http://forum.xda-developers.com/showthread.php?t=1940778
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Initialize repo:
+
+$ PATH=~/bin:$PATH; repo init -u git://github.com/androidarmv6/android.git -b cm-10.1
+$ curl --create-dirs -L -o .repo/local_manifests/local_manifest.xml -O -L https://raw.github.com/Luzifer1984/android_device_htc_chacha/cm-10.1/extra/roomservice.xml
+$ repo sync
+
+
+
+then diff and patch. Take a look at ./device/htc/chacha/extra/Modified4ChaCha.patch
+(Add support for ChaCha,thanks for adumont's patch)
+I recommend that you don't use "patch -p1" to do this .
+plz manually change these files.
+
+
+  -	android_packages_app_Camera
+
+
+OK,let's
+
+Compile:
+
+$ prebuilts/misc/linux-x86/ccache/ccache -M 50G
+
+$ source build/envsetup.sh; ./vendor/cm/get-prebuilts; USE_CCACHE=1
+  
+$ lunch cm_marvel-userdebug
+
+$ make -j4 bacon
+
+$ adb push out/target/product/DEVICENAME/cm-VERSION-DEVICENAME.zip /mnt/sdcard/
+
+Enjoy!
+
+* CREDITS
+
+
+  Google
+  Android
+  CyanogenMod
+  androidarmv6
+  OWLProject
+  Andreas Schneider
+  Special thank to winsuk
+  Special thank to JDevs
+  alquez
+  Droste
+  pabloPL
+  WFS:     crossfire77  rallapag
+  ChaCha:  adumont      Lucas Arran
+  everyone who's been working on CM10.1
+  And you!
+
+EOF
+---
---
+++ device_chacha.mk
@@ -0,0 +1,106 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/languages_full.mk)
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+$(call inherit-product, device/htc/msm7x27-common/msm7x27.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/chacha/overlay
+
+### Touchscreen
+PRODUCT_COPY_FILES += \
+    device/htc/chacha/idc/cy8c-touchscreen.idc:system/usr/idc/cy8c-touchscreen.idc
+
+### Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+### Mount vold
+PRODUCT_COPY_FILES += \
+    device/htc/chacha/vold.fstab:system/etc/vold.fstab
+
+
+### Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/chacha/key/chacha-keypad.kl:system/usr/keylayout/chacha-keypad.kl \
+    device/htc/chacha/key/chacha-keypad.kcm:system/usr/keychars/chacha-keypad.kcm \
+    device/htc/chacha/key/chacha-keypad-ara.kl:system/usr/keylayout/chacha-keypad-ara.kl \
+    device/htc/chacha/key/chacha-keypad-ara.kcm:system/usr/keychars/chacha-keypad-ara.kcm \
+    device/htc/chacha/key/chacha-keypad-aw.kl:system/usr/keylayout/chacha-keypad-aw.kl \
+    device/htc/chacha/key/chacha-keypad-aw.kcm:system/usr/keychars/chacha-keypad-aw.kcm \
+    device/htc/chacha/key/chacha-keypad-bopomo.kl:system/usr/keylayout/chacha-keypad-bopomo.kl \
+    device/htc/chacha/key/chacha-keypad-bopomo.kcm:system/usr/keychars/chacha-keypad-bopomo.kcm \
+    device/htc/chacha/key/chacha-keypad-cn.kl:system/usr/keylayout/chacha-keypad-cn.kl \
+    device/htc/chacha/key/chacha-keypad-cn.kcm:system/usr/keychars/chacha-keypad-cn.kcm \
+    device/htc/chacha/key/chacha-keypad-ell.kl:system/usr/keylayout/chacha-keypad-ell.kl \
+    device/htc/chacha/key/chacha-keypad-ell.kcm:system/usr/keychars/chacha-keypad-ell.kcm \
+    device/htc/chacha/key/chacha-keypad-esn.kl:system/usr/keylayout/chacha-keypad-esn.kl \
+    device/htc/chacha/key/chacha-keypad-esn.kcm:system/usr/keychars/chacha-keypad-esn.kcm \
+    device/htc/chacha/key/chacha-keypad-fra.kl:system/usr/keylayout/chacha-keypad-fra.kl \
+    device/htc/chacha/key/chacha-keypad-fra.kcm:system/usr/keychars/chacha-keypad-fra.kcm \
+    device/htc/chacha/key/chacha-keypad-ger.kl:system/usr/keylayout/chacha-keypad-ger.kl \
+    device/htc/chacha/key/chacha-keypad-ger.kcm:system/usr/keychars/chacha-keypad-ger.kcm \
+    device/htc/chacha/key/chacha-keypad-hk.kl:system/usr/keylayout/chacha-keypad-hk.kl \
+    device/htc/chacha/key/chacha-keypad-hk.kcm:system/usr/keychars/chacha-keypad-hk.kcm \
+    device/htc/chacha/key/chacha-keypad-ita.kl:system/usr/keylayout/chacha-keypad-ita.kl \
+    device/htc/chacha/key/chacha-keypad-ita.kcm:system/usr/keychars/chacha-keypad-ita.kcm \
+    device/htc/chacha/key/chacha-keypad-nor.kl:system/usr/keylayout/chacha-keypad-nor.kl \
+    device/htc/chacha/key/chacha-keypad-nor.kcm:system/usr/keychars/chacha-keypad-nor.kcm \
+    device/htc/chacha/key/chacha-keypad-rus.kl:system/usr/keylayout/chacha-keypad-rus.kl \
+    device/htc/chacha/key/chacha-keypad-rus.kcm:system/usr/keychars/chacha-keypad-rus.kcm \
+    device/htc/chacha/key/chacha-keypad-steng.kl:system/usr/keylayout/chacha-keypad-steng.kl \
+    device/htc/chacha/key/chacha-keypad-steng.kcm:system/usr/keychars/chacha-keypad-steng.kcm \
+    device/htc/chacha/key/chacha-keypad-tai.kl:system/usr/keylayout/chacha-keypad-tai.kl \
+    device/htc/chacha/key/chacha-keypad-tai.kcm:system/usr/keychars/chacha-keypad-tai.kcm \
+    device/htc/chacha/key/chacha-keypad-tur.kl:system/usr/keylayout/chacha-keypad-tur.kl \
+    device/htc/chacha/key/chacha-keypad-tur.kcm:system/usr/keychars/chacha-keypad-tur.kcm \
+    device/htc/chacha/key/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/chacha/key/cy8c-touchscreen.kl:system/usr/keylayout/cy8c-touchscreen.kl
+
+## Temp fix for Akmd
+PRODUCT_COPY_FILES += \
+    device/htc/chacha/bin/linker:system/bin/linker
+
+### Ramdisk
+PRODUCT_COPY_FILES += \
+    device/htc/chacha/init.chacha.rc:root/init.chacha.rc \
+    device/htc/chacha/ueventd.chacha.rc:root/ueventd.chacha.rc
+
+# For emmc phone storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.phone_storage=0
+
+### Add additional packages
+PRODUCT_PACKAGES += \
+    Email2 \
+    Exchange2 \
+    Torch
+
+# Add device specific modules ## Build for now from msm7x27-common
+#PRODUCT_PACKAGES += \
+#    gps.chacha \
+#    librpc
+
+## Get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/chacha/chacha-vendor.mk)
+
+
