4b750e15398117de42a61b0a82842600fd0ca3cb
Luzifer1984 <luzifer1984@gmail.com>
2013-07-21 22:14:50 +0800

    Update Android.mk

--- libgps/Android.mk
+++ libgps/Android.mk
@@ -13,9 +13,6 @@
 # limitations under the License.
 #
 
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libloc_intermediates)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libloc_intermediates/export_includes)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
